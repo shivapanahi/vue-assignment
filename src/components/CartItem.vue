688ea3a62361317c90225b5ed2db4505a5c1299c
sh.panahi <sh.panahi@tosantechno.com>
2024-11-24 18:23:08 +0330

    Refactor: extracting cartItems and cartSummary component

---
+++ src/components/CartItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="cart-item">
+    <img :src="item.thumbnail" :alt="item.title" class="cart-item-image" />
+    <div class="cart-item-info">
+      <h2 class="cart-item-title">{{ item.title }}</h2>
+      <p class="cart-item-price">Price: ${{ item.price }} × {{ item.quantity }}</p>
+      <div class="quantity-controls">
+        <button @click="$emit('decrease', item.id)" class="quantity-btn">-</button>
+        <span class="quantity">{{ item.quantity }}</span>
+        <button @click="$emit('increase', item.id)" class="quantity-btn">+</button>
+      </div>
+    </div>
+    <button @click="$emit('remove', item.id)" class="remove-btn">Remove</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
---
+++ src/components/CartSummary.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="cart-summary">
+      <p>Total Price: <span class="total-price">${{ totalPrice }}</span></p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      totalPrice: {
+        type: Number,
+        required: true,
+      },
+    },
+  };
+  </script>
+
+  
